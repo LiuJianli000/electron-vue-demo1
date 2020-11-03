0a7bb82ea9297a6b661fd2a1a2118db06c8d8e7e
LiuJianli000 <376122478@qq.com>
2020-11-03 16:38:57 +0800

    layout

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <div class="appMenuBar">
+
+    </div>
+    <div class="appContainer">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -10,6 +15,31 @@
   }
 </script>
 
-<style>
-  /* CSS */
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body { font-family: 'Source Sans Pro', sans-serif; }
+
+  #app {
+    background: #F5F5F5;
+    height: 100vh;
+    display: flex;
+
+    .appMenuBar {
+      width: 66px;
+      border: 1px solid red;
+      height: 100vh;
+    }
+
+    .appContainer {
+      width: 100%;
+      height: 100vh;
+    }
+  }
 </style>
---
+++ src/renderer/components/Layout/CloseOp.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="closeOp">
+    hello
+  </div>
+</template>
+
+<script>
+  import { mapActions, mapMutations, mapState } from 'vuex'
+
+  export default {
+    name: 'closeOp',
+    computed: {
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style>
+
+</style>
